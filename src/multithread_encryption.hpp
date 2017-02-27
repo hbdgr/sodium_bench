e740f19e64c74f0f8f937fb14e9cca33ff35f925
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-02-27 16:56:36 +0100

    wip

--- src/multithread_encryption.hpp
+++ src/multithread_encryption.hpp
@@ -78,68 +78,90 @@ std::unique_ptr<static_container> static_container::m_m(nullptr);
 
 
 //static void BM_threaded_onetimeAuth(benchmark::State& state) {
+//	while (state.KeepRunning()) {
+
+//		static_container &mbag = static_container::get_m(state.threads, state.range(0));
+
+//		// not run thread if is not necessary
+//		if (mbag.if_finish.at(state.thread_index) == true) {
+//			return;
+//		}
+//		auto start = std::chrono::high_resolution_clock::now();
+
+//		mbag.cipher.at(state.thread_index) =
+//		crypto_onetimeauth(reinterpret_cast<const unsigned char *> mbag.cipher.at(state.thread_index), reinterpret_cast<const unsigned char *>(msg.c_str()), msg_len, key);
+
+
+//		auto end = std::chrono::high_resolution_clock::now();
+
+//		mbag.if_finish.at(state.thread_index) = true;
+
+//		auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+//		state.SetIterationTime(elapsed_seconds.count());
+//	}
+//}
 
 
 static void BM_threaded_auth_encrypt(benchmark::State& state) {
 
+	static_container &mbag = static_container::get_m(state.threads, state.range(0));
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 	if (state.thread_index == 0) {
 		// Setup code here.
 	}
+	if (mbag.if_finish.at(state.thread_index) == true) {
+		;
+	} else {
 
-	while (state.KeepRunning()) {
+		while (state.KeepRunning()) {
 
-		static_container &mbag = static_container::get_m(state.threads, state.range(0));
+			start = std::chrono::high_resolution_clock::now();
+			// not run thread if is not necessary
 
-		// not run thread if is not necessary
-		if (mbag.if_finish.at(state.thread_index) == true) {
-			return;
+			mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
+															  mbag.nonce,
+															  mbag.bob_keys.public_key,
+															  mbag.alice_keys.secret_key);
+
+			mbag.if_finish.at(state.thread_index) = true;
+			end = std::chrono::high_resolution_clock::now();
+			auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+			state.SetIterationTime(elapsed_seconds.count());
 		}
+	}
 
-		auto start = std::chrono::high_resolution_clock::now();
-
-		mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
-														  mbag.nonce,
-														  mbag.bob_keys.public_key,
-														  mbag.alice_keys.secret_key);
-
-		auto end = std::chrono::high_resolution_clock::now();
-
-		mbag.if_finish.at(state.thread_index) = true;
-
-		auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-		state.SetIterationTime(elapsed_seconds.count());
-
-		if (state.thread_index == 0) {
-			bool all_finish = false;
-			while(!all_finish) {
-				for (const auto &b : mbag.if_finish) {
-					all_finish = true;
-					if(b == false) {
-						all_finish = false;
-					}
+	if (state.thread_index == 0) {
+		bool all_finish = false;
+		while(!all_finish) {
+			for (const auto &b : mbag.if_finish) {
+				all_finish = true;
+				if(b == false) {
+					all_finish = false;
 				}
-				std::this_thread::yield();
 			}
-			mbag.clear_container();
+			std::this_thread::yield();
 		}
+		mbag.clear_container();
 	}
 }
 
 static void BM_threaded_auth_encrypt_decrypt(benchmark::State& state) {
 
+	static_container &mbag = static_container::get_m(state.threads, state.range(0));
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 	if (state.thread_index == 0) {
 		// Setup code here.
 	}
 
 	while (state.KeepRunning()) {
-		static_container &mbag = static_container::get_m(state.threads, state.range(0));
 
+		//start = std::chrono::high_resolution_clock::now();
 		// not run thread if is not necessary
 		if (mbag.if_finish.at(state.thread_index) == true) {
-			return;
+			//state.SetIterationTime(0);
+			break;
 		}
 
-		auto start = std::chrono::high_resolution_clock::now();
 		mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
 															   mbag.nonce,
 															   mbag.bob_keys.public_key,
@@ -149,36 +171,35 @@ static void BM_threaded_auth_encrypt_decrypt(benchmark::State& state) {
 																	 mbag.nonce,
 																	 mbag.bob_keys.public_key,
 																	 mbag.alice_keys.secret_key);
-		auto end = std::chrono::high_resolution_clock::now();
-
 		mbag.if_finish.at(state.thread_index) = true;
 
-		auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-		state.SetIterationTime(elapsed_seconds.count());
+		//end = std::chrono::high_resolution_clock::now();
+		//auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+		//state.SetIterationTime(elapsed_seconds.count());
+	}
 
-		if (state.thread_index == 0) {
-			bool all_finish = false;
-			while(!all_finish) {
-				for (const auto &b : mbag.if_finish) {
-					all_finish = true;
-					if(b == false) {
-						all_finish = false;
-					}
+	if (state.thread_index == 0) {
+		bool all_finish = false;
+		while(!all_finish) {
+			for (const auto &b : mbag.if_finish) {
+				all_finish = true;
+				if(b == false) {
+					all_finish = false;
 				}
-				std::this_thread::yield();
 			}
-			std::vector<char> msg_concentrate;
-			msg_concentrate.reserve(mbag.msg.size());
-			for(auto &i : mbag.check_result) {
-				msg_concentrate.insert(msg_concentrate.end(), i.begin(), i.end());
-			}
-
-			// main check
-			if (mbag.msg != msg_concentrate) {
-				std::cout << "FAIL TO CORRECTLY DECRYPT ;/\n";
-			}
-			mbag.clear_container();
+			std::this_thread::yield();
 		}
+		std::vector<char> msg_concentrate;
+		msg_concentrate.reserve(mbag.msg.size());
+		for(auto &i : mbag.check_result) {
+			msg_concentrate.insert(msg_concentrate.end(), i.begin(), i.end());
+		}
+
+		// main check
+		if (mbag.msg != msg_concentrate) {
+			std::cout << "FAIL TO CORRECTLY DECRYPT ;/\n";
+		}
+		mbag.clear_container();
 	}
 }
 
