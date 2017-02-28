cfcdabeb2e1067e1312b03b221e2bc60f6429ea1
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-02-28 16:27:58 +0100

    Separate static container, fix bugs

--- src/multithread_encryption.hpp
+++ src/multithread_encryption.hpp
@@ -3,78 +3,9 @@
 
 #include <iostream>
 #include <thread>
-#include <memory>
-#include <atomic>
-#include <mutex>
 #include "utils.hpp"
 #include "auth_encryption.hpp"
-
-constexpr size_t Max_Threads = 128;
-
-struct static_container {
-	static std::mutex mtx;
-	static std::mutex mtx_clean;
-	std::array<std::atomic<bool>,Max_Threads> if_finish;
-
-	cryptobox_keypair alice_keys;
-	cryptobox_keypair bob_keys;
-
-	std::vector<char> msg;
-	std::vector<std::vector<char>> splitted_msg;
-	std::vector<std::vector<char>> cipher;
-	std::vector<std::vector<char>> check_result;
-
-	std::array<char, crypto_box_NONCEBYTES> nonce;
-
-	static static_container& get_m(size_t threads, size_t msg_length) {
-		std::lock_guard<std::mutex> lock(mtx);
-		if(m_m == nullptr) {
-			if (sodium_init() == -1) {
-				throw std::runtime_error("Fail to init sodium");
-			}
-			m_m = std::unique_ptr<static_container>(new static_container(threads, msg_length));
-		}
-		return *m_m;
-	}
-	volatile void clear_container() {
-		std::lock_guard<std::mutex> lock(mtx_clean);
-		if (m_m != nullptr) {
-			msg.clear();
-			splitted_msg.clear();
-			cipher.clear();
-			check_result.clear();
-
-			m_m.reset(nullptr);
-		}
-	}
-private:
-	static_container(static_container const&) = delete;
-	void operator=(static_container const&) = delete;
-
-	static_container(size_t threads, size_t msg_length) {
-		for(size_t i = 0; i < Max_Threads; ++i) {
-
-			// not run threds if is not necessary
-			i < threads && i < msg_length  ? if_finish.at(i) = false
-											: if_finish.at(i) = true;
-		}
-
-		alice_keys = generate_kyepair();
-		bob_keys = generate_kyepair();
-		nonce = generate_random_char_array<crypto_box_NONCEBYTES>();
-
-		msg = generate_random_char_vector(msg_length);
-
-		splitted_msg = split_char_vector(msg, threads);
-		cipher.resize(threads);
-		check_result.resize(threads);
-
-	}
-	static std::unique_ptr<static_container> m_m;
-};
-std::mutex static_container::mtx;
-std::mutex static_container::mtx_clean;
-std::unique_ptr<static_container> static_container::m_m(nullptr);
+#include "static_container.hpp"
 
 
 //static void BM_threaded_onetimeAuth(benchmark::State& state) {
@@ -101,7 +32,6 @@ std::unique_ptr<static_container> static_container::m_m(nullptr);
 //	}
 //}
 
-
 static void BM_threaded_auth_encrypt(benchmark::State& state) {
 
 	static_container &mbag = static_container::get_m(state.threads, state.range(0));
@@ -109,28 +39,29 @@ static void BM_threaded_auth_encrypt(benchmark::State& state) {
 	if (state.thread_index == 0) {
 		// Setup code here.
 	}
+
+	// do nothing if thread is not necessary
 	if (mbag.if_finish.at(state.thread_index) == true) {
-		;
-	} else {
-
-		while (state.KeepRunning()) {
-
-			start = std::chrono::high_resolution_clock::now();
-			// not run thread if is not necessary
-
-			mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
-															  mbag.nonce,
-															  mbag.bob_keys.public_key,
-															  mbag.alice_keys.secret_key);
-
-			mbag.if_finish.at(state.thread_index) = true;
-			end = std::chrono::high_resolution_clock::now();
-			auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-			state.SetIterationTime(elapsed_seconds.count());
-		}
+		state.SkipWithError("To many threads for small data");
 	}
 
+	start = std::chrono::high_resolution_clock::now();
+	while (state.KeepRunning()) {
+
+		mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
+																   mbag.nonce,
+																   mbag.bob_keys.public_key,
+																   mbag.alice_keys.secret_key);
+		benchmark::ClobberMemory();
+	}
+	mbag.if_finish.at(state.thread_index) = true;
+	end = std::chrono::high_resolution_clock::now();
+	auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+	state.SetIterationTime(elapsed_seconds.count());
+
 	if (state.thread_index == 0) {
+
+		// Setup code here.
 		bool all_finish = false;
 		while(!all_finish) {
 			for (const auto &b : mbag.if_finish) {
@@ -153,14 +84,13 @@ static void BM_threaded_auth_encrypt_decrypt(benchmark::State& state) {
 		// Setup code here.
 	}
 
-	while (state.KeepRunning()) {
+	// do nothing if thread is not necessary
+	if (mbag.if_finish.at(state.thread_index) == true) {
+		state.SkipWithError("To many threads for small data");
+	}
 
-		//start = std::chrono::high_resolution_clock::now();
-		// not run thread if is not necessary
-		if (mbag.if_finish.at(state.thread_index) == true) {
-			//state.SetIterationTime(0);
-			break;
-		}
+	start = std::chrono::high_resolution_clock::now();
+	while (state.KeepRunning()) {
 
 		mbag.cipher.at(state.thread_index) = cryptobox_encrypt(mbag.splitted_msg.at(state.thread_index),
 															   mbag.nonce,
@@ -171,12 +101,12 @@ static void BM_threaded_auth_encrypt_decrypt(benchmark::State& state) {
 																	 mbag.nonce,
 																	 mbag.bob_keys.public_key,
 																	 mbag.alice_keys.secret_key);
-		mbag.if_finish.at(state.thread_index) = true;
-
-		//end = std::chrono::high_resolution_clock::now();
-		//auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-		//state.SetIterationTime(elapsed_seconds.count());
+		benchmark::ClobberMemory();
 	}
+	mbag.if_finish.at(state.thread_index) = true;
+	end = std::chrono::high_resolution_clock::now();
+	auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+	state.SetIterationTime(elapsed_seconds.count());
 
 	if (state.thread_index == 0) {
 		bool all_finish = false;
---
+++ src/static_container.hpp
@@ -0,0 +1,74 @@
+#ifndef STATIC_CONTAINER_HPP
+#define STATIC_CONTAINER_HPP
+
+#include <mutex>
+#include <memory>
+#include <atomic>
+
+constexpr size_t Max_Threads = 64;
+
+struct static_container {
+	static std::mutex mtx;
+	static std::mutex mtx_clean;
+	static std::array<std::atomic<bool>,Max_Threads> if_finish;
+
+	cryptobox_keypair alice_keys;
+	cryptobox_keypair bob_keys;
+
+	std::vector<char> msg;
+	std::vector<std::vector<char>> splitted_msg;
+	std::vector<std::vector<char>> cipher;
+	std::vector<std::vector<char>> check_result;
+
+	std::array<char, crypto_box_NONCEBYTES> nonce;
+
+	static static_container& get_m(size_t threads, size_t msg_length) {
+		std::lock_guard<std::mutex> lock(mtx);
+		if(m_m == nullptr) {
+			if (sodium_init() == -1) {
+				throw std::runtime_error("Fail to init sodium");
+			}
+			m_m = std::unique_ptr<static_container>(new static_container(threads, msg_length));
+		}
+		return *m_m;
+	}
+	volatile void clear_container() {
+		std::lock_guard<std::mutex> lock(mtx_clean);
+		if (m_m != nullptr) {
+			msg.clear();
+			splitted_msg.clear();
+			cipher.clear();
+			check_result.clear();
+
+			m_m.reset(nullptr);
+		}
+	}
+private:
+	static_container(static_container const&) = delete;
+	void operator=(static_container const&) = delete;
+
+	static_container(size_t threads, size_t msg_length) {
+		for(size_t i = 0; i < Max_Threads; ++i) {
+			// not run threds if is not necessary
+			i < threads && i < msg_length  ? if_finish.at(i) = false
+											: if_finish.at(i) = true;
+		}
+
+		alice_keys = generate_kyepair();
+		bob_keys = generate_kyepair();
+		nonce = generate_random_char_array<crypto_box_NONCEBYTES>();
+
+		msg = generate_random_char_vector(msg_length);
+
+		splitted_msg = split_char_vector(msg, threads);
+		cipher.resize(threads);
+		check_result.resize(threads);
+	}
+	static std::unique_ptr<static_container> m_m;
+};
+std::mutex static_container::mtx;
+std::mutex static_container::mtx_clean;
+std::array<std::atomic<bool>,Max_Threads> static_container::if_finish;
+std::unique_ptr<static_container> static_container::m_m(nullptr);
+
+#endif // STATIC_CONTAINER_HPP
