1ad49451f01ccf1f814633a37fb86910de0c6cf4
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-03-05 23:57:44 +0100

    calc thread creation cost, prepare for symetric encryption

--- src/main.cpp
+++ src/main.cpp
@@ -37,29 +37,56 @@ static void CustomArguments(benchmark::internal::Benchmark* b) {
 
 
 constexpr size_t Min_inputArg = 128; // should be >= than thread num
-constexpr size_t Max_inputArg = 2<<10;
-//constexpr size_t Max_inputArg = 1048576;
+//constexpr size_t Max_inputArg = 2<<4;
+constexpr size_t Max_inputArg = 1048576;
 
 //BENCHMARK(BM_crypto_single_onetimeAuth)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg);
 //BENCHMARK(BM_crypto_single_onetimeAuth_and_verify)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg);
 
 // anthenticated encryption
-BENCHMARK(BM_threaded_auth_encrypt)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg)
-		  ->Threads(1)->Threads(2)->Threads(4)->Threads(6)->Threads(8)
+
+static void calc_thread_cost(benchmark::State& state) {
+	int threads = 10000;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (volatile int i = 0; i < threads; i++) {
+		std::thread r([](){});
+		r.join();
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	thread_cost = (std::chrono::duration_cast<std::chrono::duration<double>>(end - start))/threads;
+	while (state.KeepRunning()) {
+		std::thread r([](){});
+		r.join();
+	}
+}
+
+//BENCHMARK(run_unit_tests);
+BENCHMARK(calc_thread_cost);
+
+BENCHMARK(BM_multithread_xsalsa_sym_encrypt)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg)
+		  ->Threads(1)->Threads(2)->Threads(4)->Threads(8)
 		  ->Threads(12)->Threads(16)->Threads(32)->Threads(48)
+		  ->Threads(128)
 		  ->UseManualTime();
+
+//BENCHMARK(BM_threaded_auth_encrypt)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg)
+//		  ->Threads(1)->Threads(2)->Threads(4)->Threads(8)
+//		  ->Threads(12)->Threads(16)->Threads(32)->Threads(48)
+//		  ->Threads(128)
+//		  ->UseManualTime();
 
 //// anthenticated encryption and decryption
 BENCHMARK(BM_threaded_auth_encrypt_decrypt)->RangeMultiplier(2)->Range(Min_inputArg, Max_inputArg)
-		  ->Threads(1)->Threads(2)->Threads(4)->Threads(6)->Threads(8)
+		  ->Threads(1)->Threads(2)->Threads(4)->Threads(8)
 		  ->Threads(12)->Threads(16)->Threads(32)->Threads(48)
+		  ->Threads(128)
 		  ->UseManualTime();
 
 BENCHMARK(BM_staticData_encrypt_decrypt)->RangeMultiplier(2)->Range(Min_inputArg,Max_inputArg)
-		  ->Threads(1)->Threads(2)->Threads(4)->Threads(6)->Threads(8)
+		  ->Threads(1)->Threads(2)->Threads(4)->Threads(8)
 		  ->Threads(12)->Threads(16)->Threads(32)->Threads(48)
+		  ->Threads(128)
 		  ->UseManualTime();
 
-//BENCHMARK(run_unit_tests);
 
 BENCHMARK_MAIN()
